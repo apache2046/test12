7195b6830889a7a382ad9c3bbd93ae73892264c8
apache2046 <apache2046@gmail.com>
2014-12-15 21:55:13 +0800

    Initial Commit

---
+++ t4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  t4
+//
+//  Created by Gao Haibin on 14/12/15.
+//  Copyright (c) 2014年 Gao Haibin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
